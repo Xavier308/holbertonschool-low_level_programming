7824328c529d3fa774ca154d02ccb63c9498a9d2
Xavier308 <xavvis308@gmail.com>
2024-02-23 17:59:40 -0400

    function that prints the alphabet, in lowercase, followed by a new line

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-void times_table(void);  /*  Prototype for times_table */
-
+void times_table(void);/* Prototype for times_table */
+void print_alphabet(void);/* Prototype */
 #endif /* MAIN_H */
