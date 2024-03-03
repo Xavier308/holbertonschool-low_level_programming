12b186c3d68acc3fdff1c4adb38fecbcb8bfebc4
Xavier308 <xavvis308@gmail.com>
2024-03-02 20:21:39 -0400

    function that copies a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,16 +14,20 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	int index = 0;
 
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
+	while (index < n && src[index] != '\0')
+	{
 		dest[index] = src[index];
+		index++;
+	}
 
-	for (index = src_len; index < n; index++)
+
+	while (index < n)
+	{
 		dest[index] = '\0';
+		index++;
+	}
 
 	return (dest);
 }
