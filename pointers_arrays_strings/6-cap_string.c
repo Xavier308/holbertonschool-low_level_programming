0a52b3d1b75e98eea0c49c4e56190accd4c90ff3
Xavier308 <xavvis308@gmail.com>
2024-03-02 20:43:53 -0400

    function that capitalizes all words of a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,54 +1,37 @@
 #include "main.h"
 
 /**
- * is_separator - Checks if character is a separator.
- * @c: The character to check.
- *
- * Return: 1 if character is a separator, 0 otherwise.
- */
-int is_separator(char c)
-{
-	char separators[] = " \t\n,;.!?\"(){}";
-	int i = 0;
-
-	while (separators[i])
-	{
-		if (c == separators[i])
-		{
-		return (1);
-		}
-		i++;
-		}
-		return (0);
-	}
-
-/**
  * cap_string - Capitalizes all words of a string.
- * @str: The string to be modified.
+ * @str: The string to be capitalized.
  *
- * Return: The modified string.
+ * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
 {
-	int i = 0;
-	int capitalize_next = 1;
+int index = 0;
 
-	while (str[i])
+	while (str[index])
 	{
-		if (is_separator(str[i]))
-		{
-			capitalize_next = 1;
-		}
-		else if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] -= 'a' - 'A';
-			capitalize_next = 0;
-		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			capitalize_next = 0;
-		}
-		i++;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
 
 	return (str);
