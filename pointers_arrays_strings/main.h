b34bef91c88a55b6081a431b6c468bc46b45b9b7
Xavier308 <xavvis308@gmail.com>
2024-03-06 15:30:42 -0400

    function that fills memory with a constant byte

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,4 +25,16 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 
+/* prototypes of pointers, arrays & strings */
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
+/* prototypes of recursions */
+
 #endif /* MAIN_H */
